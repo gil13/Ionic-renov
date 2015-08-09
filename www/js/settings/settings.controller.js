4dae185131f67c380fa100e57bd021f0a7ef1ec8
Alberto Gil <alberto.gil.contractor@bbva.com>
2015-08-09 23:25:38 +0200

    First

---
+++ www/js/settings/settings.controller.js
@@ -0,0 +1,19 @@
+// (function(){
+//     'use strict';
+//
+//     angular
+//         .module('starter.controllers', [])
+//         .controller('Settings', function($scope, $ionicModal, $timeout) {
+//
+//             $ionicModal.fromTemplateUrl('js/settings/personalData.html', {
+//               scope: $scope
+//             }).then(function(modal) {
+//               $scope.modal = modal;
+//             });
+//
+//             $scope.personalData = function() {
+//               $scope.modal.show();
+//             };
+//
+//         });
+// })()
