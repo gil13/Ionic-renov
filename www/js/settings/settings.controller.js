8086dc9f0f60308b38cb0e5b6cdecdec6a5a341f
Alberto Gil <alberto.gil.contractor@bbva.com>
2015-08-10 11:17:56 +0200

    [fix] refactor and comments

--- www/js/settings/settings.controller.js
+++ www/js/settings/settings.controller.js
@@ -4,16 +4,17 @@
 //     angular
 //         .module('starter.controllers', [])
 //         .controller('Settings', function($scope, $ionicModal, $timeout) {
+//             console.log('test');
 //
-//             $ionicModal.fromTemplateUrl('js/settings/personalData.html', {
-//               scope: $scope
-//             }).then(function(modal) {
-//               $scope.modal = modal;
-//             });
-//
-//             $scope.personalData = function() {
-//               $scope.modal.show();
-//             };
+//             // $ionicModal.fromTemplateUrl('js/settings/personalData.html', {
+//             //   scope: $scope
+//             // }).then(function(modal) {
+//             //   $scope.modal = modal;
+//             // });
+//             //
+//             // $scope.personalData = function() {
+//             //   $scope.modal.show();
+//             // };
 //
 //         });
 // })()
